3b14c4b0147e3f1e783a3d3eb873ba2bc3409d24
kyoreth <gursz@gmail.com>
2018-11-22 14:36:46 +0100

    excract seqs work. moving on

--- handin3/handin3.py
+++ handin3/handin3.py
@@ -27,36 +27,9 @@ def read_fasta_file(filename):
         sequences[name] = ''.join(lines)
     return sequences
 
-gen1 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome1.fa')
-ann1 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann1.fa')
 
-gen2 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome2.fa')
-ann2 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann2.fa')
-
-gen3 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome3.fa')
-ann3 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann3.fa')
-
-gen4 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome4.fa')
-ann4 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann4.fa')
-
-gen5 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome5.fa')
-ann5 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann5.fa')
-
-
-#gen_dict =  {'genome1': [gen1,ann1],'genome2': [gen2,ann2],'genome3': [gen3,ann3],'genome4': [gen4,ann4],'genome5': [gen5,ann5]}
-gen_arr = [[gen1,ann1],[gen2,ann2],[gen3,ann3],[gen4,ann4],[gen5,ann5],]
-#print(gen_dict['genome1'][0])
-
-count_mat = np.zeros(shape=(3,3))
-print(type(ann1))
-print(list(gen_arr[0][1].items())[0][1][0])
-#for i in gen_dict:
-
-char_arr = ['C','R','N']
-
-def count_char(seq,from_char,to_char):
+def count_char(seq, from_char, to_char):
     count = 0
-    #print(seq[1:5])
     for i in range(0, len(seq)-1):
         #print(i)
         if seq[i] == from_char and seq[i+1] == to_char:
@@ -64,29 +37,102 @@ def count_char(seq,from_char,to_char):
             count += 1
     return count
 
-#print(count_char(list(ann1.items())[0][1],'R','R'))
+
+def create_count_matrix(gen_arr, char_arr):
+    count_mat = np.zeros(shape=(3, 3))
+    for x in range(0,len(gen_arr)):
+        for i in char_arr:
+            for m in char_arr:
+                count_mat[char_arr.index(i),char_arr.index(m)] += count_char(list(gen_arr[x][1].items())[0][1],i,m)
+
+
+def make_trans(count_mat):
+    trans_mat = np.zeros(shape=(3, 3))
+    for i in range(0,3):
+        for x in range(0,3):
+            trans_mat[i,x] = count_mat[i,x]/sum(count_mat[i])
+    return trans_mat
+
+
+def chk_next(ann, i):
+
+    if i == len(ann)-1:
+        return False
+    if ann[i] == ann[i+1]:
+        return True
+    return False
+
+
+def extract_seq(seq,ann):
+    i = 0
+    start = 0
+    end = 0
+    n_list = []
+    r_list = []
+    c_list = []
+    while i != len(ann):
+        if ann[i] == 'N' and not chk_next(ann, i):
+            end = i + 1
+            n_list.append(seq[start:end])
+            start = i + 1
+        if ann[i] == 'C' and not chk_next(ann, i):
+            end = i + 1
+            c_list.append(seq[start:end])
+            start = i + 1
+        if ann[i] == 'R' and not chk_next(ann, i):
+            end = i + 1
+            r_list.append(seq[start:end])
+            start = i + 1
+        i += 1
+
+    return c_list, r_list, n_list
+
+
+def test(seq, ann):
+    print('Starting Test with: ')
+    print(seq)
+    print(ann)
+    print('Extracting sequences based on annotation: ')
+    result = extract_seq(seq, ann)
+    print('N Sequences: ')
+    print(result[2])
+    print('C Sequences: ')
+    print(result[0])
+    print('R Sequences: ')
+    print(result[1])
+
+
+    print('Test succes')
+
+if __name__ == '__main__':
+    # TODO: make short seq and ann to test input, output of func
+    test(seq='CTGACGTCAGCTACGTACGACATGCGTATRC',
+         ann='NNNNNNNNNNNCCCCCCCCNNNRRRRRRCNR')
+
+    gen1 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome1.fa')
+    ann1 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann1.fa')
+
+    gen2 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome2.fa')
+    ann2 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann2.fa')
+
+    gen3 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome3.fa')
+    ann3 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann3.fa')
+
+    gen4 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome4.fa')
+    ann4 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann4.fa')
+
+    gen5 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/genome5.fa')
+    ann5 = read_fasta_file('C:/Users/Ky/Desktop/ml18/handin3/data-handin3/true-ann5.fa')
 
 
 
+    gen_arr = [[gen1,ann1],[gen2,ann2],[gen3,ann3],[gen4,ann4],[gen5,ann5]]
+    char_arr = ['C','R','N']
+    #print(list(gen_arr[0][1].items())[0][1][0])
 
-for x in range(0,len(gen_arr)):
-    for i in char_arr:
-        for m in char_arr:
-            count_mat[char_arr.index(i),char_arr.index(m)] += count_char(list(gen_arr[x][1].items())[0][1],i,m)
-
-print(count_mat)
-
-trans_mat  = np.zeros(shape=(3,3))
-
-for i in range(0,3):
-    for x in range(0,3):
-        trans_mat[i,x] = count_mat[i,x]/sum(count_mat[i])
-
-print(trans_mat)
-print([sum(trans_mat[i]) for i in range(0,3)])
-
-#print([sum(count_mat[i]/tot_len) for i in range(0,3)])
-
-
-
+    #print(list(gen_arr[0][1].items())[0][1][1:500])
+    #print(list(gen_arr[0][0].items())[0][1][1:500])
+    #result = extract_seq(list(gen_arr[0][0].items())[0][1],list(gen_arr[0][1].items())[0][1])
+    #print(result[2])
+    #print(result[0])
 
